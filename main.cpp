0b1faf7a5f04a636529ce9b99662ae0e6b3ddd24
Miler Kamil <mmiler6969@gmail.com>
2022-06-25 17:55:01 +0200

    Stworzenie struktur kolor naglowek

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
 using namespace std;
+    //Struktura kolor�w
+    struct kolor
+    {
+    char R;
+    char G;
+    char B;
+    } kolor;
 
+    //Struktura naglowek
+    struct naglowek
+    {
+    int Size;
+    int Width;
+    int Height;
+    short Planes;
+    short BitCount;
+    int Compression;
+    int SizeImage;
+    int XPelsPerMeter;
+    int YPelsPerMeter;
+    int CUsed;
+    int CImportant;
+    } zdjecie;
+
+    //Struktura naglowek plik
+    struct NaglowekPlik
+    {
+    short fType;
+    int fSize;
+    short fReserved1;
+    short fReserved2;
+    int fOffBits;
+    } Plik;
 int main()
 {
     cout << "Hello world!" << endl;
